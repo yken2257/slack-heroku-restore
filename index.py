aafd88556844fdca8dbc32a3f3985b18deb7a686
kento-yoshida <kento-yoshida.kke.co.jp>
2020-07-12 06:39:02 +0900

    port changed to 3000

--- index.py
+++ index.py
@@ -35,4 +35,4 @@ def reaction_added(event_data):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
