f3dfd00f5a8a42d5f239e8b11b2cfd60aaa30a6e
kento-yoshida <kento-yoshida.kke.co.jp>
2020-07-11 13:21:03 +0900

    add slackeventsapi library

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, jsonify, request
+from slackeventsapi import SlackEventAdapter
 #from json import dumps
 
+
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 
 app = Flask(__name__)
 
@@ -17,10 +20,19 @@ def main():
     else:
         return jsonify({'test':'test'}), 200
 
-
 @app.route('/test')
 def greed():
     return 'Hello, World!\n'
+
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
+
+
+# Create an event listener for "reaction_added" events and print the emoji name
+@slack_events_adapter.on("reaction_added")
+def reaction_added(event_data):
+  emoji = event_data["event"]["reaction"]
+  print(emoji)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
