75759a9acf1ec92af2775e7f56f986235686af3e
kento-yoshida <kento-yoshida.kke.co.jp>
2020-07-12 09:51:22 +0900

    get twitter url from conversations_replies check

--- index.py
+++ index.py
@@ -28,18 +28,23 @@ def hello():
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
 
-# Create an event listener for "reaction_added" events and print the emoji name
+# create an event listener for "reaction_added" events and print the emoji name
 @slack_events_adapter.on("reaction_added")
 def reaction_added(event_data):
     emoji = event_data["event"]["reaction"]
     channel = event_data["event"]["item"]["channel"]
     ts = event_data["event"]["item"]["ts"]
+    print(type(ts))
     text = ":%s:" % emoji
     print(emoji)
     print(event_data)
     client.chat_postMessage(channel=channel, text=text)
     convs = client.conversations_replies(channel=channel, ts=ts)
+    content = convs["messages"]["attachments"][0]
+    print(content)
     print(convs)
+    print(content["tile_link"])
+    print(content["text"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
