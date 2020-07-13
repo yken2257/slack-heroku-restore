7fcd9a65c434fc788f4e54bac1a105459252090d
kento-yoshida <kento-yoshida.kke.co.jp>
2020-07-14 07:04:59 +0900

    add updating on gspread-sheet

--- index.py
+++ index.py
@@ -3,11 +3,39 @@ from datetime import datetime, timedelta, timezone
 from flask import Flask, jsonify, request
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+#import pandas as pd
+
 #from json import dumps
 
 JST = timezone(timedelta(hours=+9), 'JST')
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+cred_dict = {
+    "type": "service_account",
+    "project_id": os.environ['SHEET_PROJECT_ID'],
+    "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+    "private_key": os.environ['SHEET_PRIVATE_KEY'].replace('\\n', '\n'),
+    "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+    "client_id": os.environ['SHEET_CLIENT_ID'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+}
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(cred_dict, scope)
+
+gc = gspread.authorize(credentials)
+spreadsheet_key = os.environ['SPREADSHEET_KEY']
+
+
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
 
 app = Flask(__name__)
 
@@ -37,7 +65,6 @@ def reaction_added(event_data):
     channel = event_data["event"]["item"]["channel"]
     ts = event_data["event"]["item"]["ts"]
     print(type(ts))
-    text = ":%s:" % emoji
     print(emoji)
     #print(event_data)
     #client.chat_postMessage(channel=channel, text=text)
@@ -45,11 +72,17 @@ def reaction_added(event_data):
     content = convs["messages"][0]["attachments"][0]
     print(content)
     #print(convs)
-    print(content["title_link"])
-    print(content["text"])
+    link = content["title_link"]
+    text = content["text"]
     dt = datetime.fromtimestamp(int(ts[:10]), JST)
     dt = f"{dt:%Y-%m-%d %H:%M:%S}"
     print(dt)
+
+    wrksht = gc.open_by_key(spreadsheet_key).sheet1
+    if emoji == '-1':
+        add_list = [dt, link, text]
+        print(add_list)
+        wrksht.append_row(add_list)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
